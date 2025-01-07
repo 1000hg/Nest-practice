d0890633a2a827c0b62eb05df8c0a390f08966d4
alxzkf123 <alxzkf123@gmail.com>
2025-01-08 04:41:09 +0900

    [refactor] typeorm 엔티티 동적호출 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,36 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from 'modules/users/user.module';
-import * as path from 'path';
-import * as fs from 'fs';
 import { BackupModule } from 'backup/backup.module';
 import { AuthModule } from 'modules/auths/auth.module';
 import config from './config/index';
-
-/** 엔티티 동적 수집 */
-function collectEntities(): string[] {
-  const baseDir = path.resolve(__dirname, 'modules');
-  const entityPaths: string[] = [];
-
-  const modules = fs.readdirSync(baseDir).filter((folder) => {
-    const modulePath = path.join(baseDir, folder, 'entities');
-    return fs.existsSync(modulePath) && fs.lstatSync(modulePath).isDirectory();
-  });
-
-  modules.forEach((module) => {
-    const entitiesDir = path.join(baseDir, module, 'entities');
-
-    const entityFiles = fs
-      .readdirSync(entitiesDir)
-      .filter((file) => file.match(/\.entity\.(ts|js)$/));
-
-    entityFiles.forEach((file) => {
-      entityPaths.push(path.join(entitiesDir, file));
-    });
-  });
-
-  return entityPaths;
-}
 
 @Module({
   imports: [
@@ -48,7 +21,7 @@ function collectEntities(): string[] {
         username: configService.get('database.username'),
         password: configService.get('database.password'),
         database: configService.get('database.database'),
-        entities: collectEntities(),
+        entities: [__dirname + '/modules/**/*{.ts,.js}'],
         synchronize: configService.get('database.synchronize'),
         logging: configService.get('database.logging'),
       }),
