80d6a18b1d80cff84625d78d168f2f3ff006a927
alxzkf123 <alxzkf123@gmail.com>
2025-01-14 04:08:05 +0900

    [feat] 프론트 디자인 추가

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,30 @@ sortBtn.addEventListener('click', () => {
   sort.classList.toggle('show');
 });
 
+const loginModal = document.getElementById('loginModal');
+const loginOpenBtn = document.getElementById('login-btn');
+const loginCloseBtn = document.getElementById('login-close');
+
+loginOpenBtn.addEventListener('click', function () {
+  loginModal.style.display = 'block';
+});
+
+loginCloseBtn.addEventListener('click', function () {
+  loginModal.style.display = 'none';
+});
+
+const signUpModal = document.getElementById('signUpModal');
+const signUpOpenBtn = document.getElementById('signup-btn');
+const signUpCloseBtn = document.getElementById('signup-close');
+
+signUpOpenBtn.addEventListener('click', function () {
+  signUpModal.style.display = 'block';
+});
+
+signUpCloseBtn.addEventListener('click', function () {
+  signUpModal.style.display = 'none';
+});
+
 window.addEventListener('click', (event) => {
   if (!category.contains(event.target)) {
     category.classList.remove('show');
@@ -19,5 +43,13 @@ window.addEventListener('click', (event) => {
 
   if (!sort.contains(event.target)) {
     sort.classList.remove('show');
+  }
+
+  if (event.target === loginModal) {
+    loginModal.style.display = 'none';
+  }
+
+  if (event.target === signUpModal) {
+    signUpModal.style.display = 'none';
   }
 });
